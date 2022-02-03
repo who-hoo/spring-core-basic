6b312dd9c15aca798560a9b0fafe81eab6b83744
who-hoo <who.ho3ov@gmail.com>
2022-02-04 01:33:12 +0900

    :recycle: 스프링 적용

--- src/main/java/study/springcorebasic/OrderApp.java
+++ src/main/java/study/springcorebasic/OrderApp.java
@@ -1,5 +1,7 @@
 package study.springcorebasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.springcorebasic.member.Grade;
 import study.springcorebasic.member.Member;
 import study.springcorebasic.member.MemberService;
@@ -9,9 +11,9 @@ import study.springcorebasic.order.OrderService;
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
