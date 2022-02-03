a3bca2b85d8e45aed4e5453b83ca96217b414623
who-hoo <who.ho3ov@gmail.com>
2022-02-03 15:36:40 +0900

    :sparkles: 새로운 할인 정책(RateDiscountPolicy) 개발

---
+++ src/main/java/study/springcorebasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,21 @@
+package study.springcorebasic.discount;
+
+import study.springcorebasic.member.Grade;
+import study.springcorebasic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    private boolean isVip(Member member) {
+        return member.getGrade() == Grade.VIP;
+    }
+
+    @Override
+    public int discount(Member member, int price) {
+        if (isVip(member)) {
+            return price * discountPercent / 100;
+        }
+        return 0;
+    }
+}
