1c0a8b7cc9bb617612446ebf3130f25b1099bfe7
who-hoo <who.ho3ov@gmail.com>
2022-02-04 01:32:51 +0900

    :recycle: 스프링 컨테이너, 스프링 빈 등록

--- src/main/java/study/springcorebasic/AppConfig.java
+++ src/main/java/study/springcorebasic/AppConfig.java
@@ -1,5 +1,7 @@
 package study.springcorebasic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import study.springcorebasic.discount.DiscountPolicy;
 import study.springcorebasic.discount.RateDiscountPolicy;
 import study.springcorebasic.member.MemberRepository;
@@ -9,20 +11,25 @@ import study.springcorebasic.member.MemoryMemberRepository;
 import study.springcorebasic.order.OrderService;
 import study.springcorebasic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
