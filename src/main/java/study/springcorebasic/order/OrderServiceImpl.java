80805d7d0f69b64ba8c7cf215079d3b1f97ed399
who-hoo <who.ho3ov@gmail.com>
2022-03-03 03:43:08 +0900

    :recycle: 롬복 라이브러리를 적용하여 생성자 자동 생성
    롬복 라이브러리가 제공하는 @RequiredArgsConstructor 기능을 사용하면 final이 붙은 필드를 모아서 생성자를 자동으로 만들어준다.

--- src/main/java/study/springcorebasic/order/OrderServiceImpl.java
+++ src/main/java/study/springcorebasic/order/OrderServiceImpl.java
@@ -1,23 +1,17 @@
 package study.springcorebasic.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import study.springcorebasic.discount.DiscountPolicy;
 import study.springcorebasic.member.Member;
 import study.springcorebasic.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository,
-        DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
