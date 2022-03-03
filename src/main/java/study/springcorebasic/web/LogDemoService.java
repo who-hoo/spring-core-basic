9238d12d55c3f0806e540aa6f99f25c4b8792124
who-hoo <who.ho3ov@gmail.com>
2022-03-04 03:08:18 +0900

    :sparkles: request 스코프 예제

---
+++ src/main/java/study/springcorebasic/web/LogDemoService.java
@@ -0,0 +1,18 @@
+package study.springcorebasic.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+import study.springcorebasic.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
+    }
+}
